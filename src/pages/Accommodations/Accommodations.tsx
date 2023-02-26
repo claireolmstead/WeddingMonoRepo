0e764c294b2f50cb38255aac6a43cf2297ed088e
Claire Olmstead <olmsteadclaire@gmail.com>
2023-02-26 12:55:56 -0800

    display accomodations warning

--- src/pages/Accommodations/Accommodations.tsx
+++ src/pages/Accommodations/Accommodations.tsx
@@ -2,7 +2,9 @@ import styled from '@emotion/styled';
 import React from 'react';
 
 import { accommodations } from '../../consts/accommodations';
+import { ScreenSizes } from '../../consts/vars';
 import AccommodationsImg from '../../images/Accommodations.png';
+import Container from '../../uiComponents/Container';
 import ListItemBlock from '../../uiComponents/ListItemBlock';
 
 const AccommodationsBackground = styled.div`
@@ -14,15 +16,26 @@ const AccommodationsBackground = styled.div`
   width: 100vw;
 `;
 
+const AccommodationsMoreInfo = styled(Container)`
+  color: ${(props) => props.theme.colors.red};
+  position: relative;
+  text-align: center;
+  top: 120px;
+
+  @media only screen and (min-width: ${ScreenSizes.MOBILE}px) {
+    top: 50px;
+  }
+`;
+
 const Accommodations = () => {
   return (
     <div>
       <AccommodationsBackground />
       <ListItemBlock list={accommodations} />
-      <div>
+      <AccommodationsMoreInfo>
         PLEASE BOOK ACCOMMODATION AS SOON AS POSSIBLE AS THERE IS{' '}
         <b>NO GUARANTEE OF AVAILABILITY</b>.
-      </div>
+      </AccommodationsMoreInfo>
     </div>
   );
 };
